8a8ffd68e837c467be6d780f48a1c3f0b79c4145
David Román <david.roman@mykukun.com>
2022-03-29 01:39:09 -0500

    add options

--- src/App.js
+++ src/App.js
@@ -17,10 +17,15 @@ const slides = [
     text: "Landscape Maintenance",
     img: "https://content.mykukun.com/wp-content/uploads/2021/02/24113301/landscape-maintenance.jpg",
   },
+
 ];
 
 function App() {
   const [height, setHeight] = useState(400);
+  const [autoplay, setAutoplay] = useState(true);
+  const [dots, setDots] = useState(true);
+  const [arrowOpacity, setArrowOpacity] = useState(99);
+  const [speed, setSpeed] = useState(4)
 
   return (
     <Container fluid>
@@ -28,16 +33,55 @@ function App() {
         <Col>
           <h2>Carousel</h2>
           <hr />
-          <Carousel height={height} slides={slides}></Carousel>
+          <Carousel
+            height={height}
+            slides={slides}
+            boxSpacing={false}
+            arrowOpacity={arrowOpacity}
+            autoplay={autoplay}
+            dots={dots}
+            draggable={false}
+            speed={speed}
+          ></Carousel>
         </Col>
         <Col md={4}>
           <h2>Options</h2>
           <hr />
+          <Form.Label>arrowOpacity: {arrowOpacity}%</Form.Label>
+          <Form.Range
+            value={arrowOpacity}
+            onChange={({ target }) => {
+              if (target.value < 100) setArrowOpacity(target.value);
+            }}
+          />
+          <Form.Label>height: {height} px</Form.Label>
           <Form.Control
             min="0"
             max="800"
             value={height}
             onChange={({ target }) => setHeight(target.value)}
+            type="number"
+          ></Form.Control>
+          <Form.Check
+            className="py-2"
+            type="switch"
+            checked={autoplay}
+            onChange={({ target }) => setAutoplay(target.checked)}
+            label={"autoplay - " + autoplay}
+          />
+          <Form.Check
+            className="py-2"
+            type="switch"
+            checked={dots}
+            onChange={({ target }) => setDots(target.checked)}
+            label={"dots - " + dots}
+          />
+          <Form.Label>speed: {speed} Sec.</Form.Label>
+          <Form.Control
+            min="1"
+            max="10"
+            value={speed}
+            onChange={({ target }) => setSpeed(target.value)}
             type="number"
           ></Form.Control>
         </Col>
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,14 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function Carousel({ slides, height }) {
+function Carousel({ slides, height, autoplay, dots, arrowOpacity, speed }) {
   const [current, setCurrent] = useState(0);
+  const [hover, setHover] = useState(false);
+  const [drag, setDrag] = useState(false);
+
+  const changeSlide = (up) => {
+    let type = up ? current + 1 : current - 1;
+    if (up && current === slides.length - 1) type = 0;
+    if (!up && current === 0) type = slides.length - 1;
+    setCurrent(type);
+  };
+
+  const onDrag = (e) => {
+    if (drag) changeSlide(e.movementX < 0);
+  };
+
+  useEffect(() => {
+    if (!autoplay) return;
+    if (!hover) return setTimeout(() => changeSlide(true), speed * 1000);
+  }, [current, autoplay, hover]);
 
   return (
-    <div className="k-carousel" tabIndex="-1">
+    <div
+      className="k-carousel"
+      tabIndex="-1"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => {
+        setHover(false);
+        setDrag(false);
+      }}
+      onMouseDown={() => setDrag(true)}
+      onMouseUp={() => setDrag(false)}
+    >
       <button
-        disabled={current === 0}
-        onClick={() => setCurrent(current - 1)}
+        onClick={() => changeSlide(false)}
         type="button"
+        style={{ opacity: "0." + arrowOpacity }}
         className="k-carousel-arrow k-carousel-arrow-prev"
       >
         <i className="k-icon-arrow-left-bold k-icon-color-primary-accent"></i>
@@ -19,38 +47,47 @@ function Carousel({ slides, height }) {
       >
         {slides.map(({ img, text }, i) => {
           return (
-            <div key={i}
+            <div
+              key={i}
               style={{
-                left: i - current + "00%",
+                left: (i - current) * 100 + "%",
                 backgroundImage: `url(${img})`,
               }}
+              onMouseMove={(e) => onDrag(e)}
+              onTouchMove={(e) => onDrag(e)}
               className="k-react-carousel-box k-width-100"
             >
-              <h1>{text}</h1>
+              <h1>
+                current: {autoplay ? "true" : "false"}
+                {100 / arrowOpacity}
+              </h1>
             </div>
           );
         })}
       </div>
       <button
-        disabled={current === slides.length - 1}
-        onClick={() => setCurrent(current + 1)}
+        onClick={() => changeSlide(true)}
         type="button"
+        style={{ opacity: "0." + arrowOpacity }}
         className="k-carousel-arrow k-carousel-arrow-next"
       >
         <i className="k-icon-arrow-right-bold k-icon-color-primary-accent"></i>
       </button>
-      <ul className="k-carousel-dots-wrapper">
-        {slides.map((_, i) => {
-          const active = current === i ? { backgroundColor: "#fc4d12" } : {};
-          return (
-            <li key={i}
-              onClick={() => setCurrent(i)}
-              style={active}
-              className="k-carousel-dot"
-            ></li>
-          );
-        })}
-      </ul>
+      {dots && (
+        <ul className="k-carousel-dots-wrapper">
+          {slides.map((_, i) => {
+            const active = current === i ? { backgroundColor: "#fc4d12" } : {};
+            return (
+              <li
+                key={i}
+                onClick={() => setCurrent(i)}
+                style={active}
+                className="k-carousel-dot"
+              ></li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
